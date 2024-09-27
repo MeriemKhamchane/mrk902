fa681163cc5477ab711ea99fd88633f16082ee42
MeriemKhamchane <mrk902@uowmail.edu.au>
2024-09-27 12:24:43 +0400

    Add files via upload

---
+++ LAB 2 Progress.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+#define beans = 8
+#define water_mocha = 39
+#define water_capuccino = 30
+#define water_espresso = 30
+#define milk_mocha = 39
+#define milk_mocha = 39
+#define chocolate_syrup = 36
+
+
+int main() {
+
+    int choice = -1 // when the user inputs 1
+    
+    printf("MENU:\n1. Espresso\n2. Cappucino\n3. Mocha\n4. Exit");
+    function espresso
+    function cappucino
+    function mocha
+    function payment
+    
+    
+    int choice = -2
+    
+    printf("Enter admin password:");
+        if inncorrect pasword
+        printf("Incorrect password entered please try again");
+        //reprint "Enter admin password:" using infinite loop maybe with continue?
+        
+        else correct password
+        //end infinite loop maybe with break?
+
+        printf("MENU:\n1. Check pantry and sales\n2. Replenish ingredients\n3. Change coffee price\n4. Exit");
+            function check pantry
+            function check sales
+            function replensih ingredients
+            function change coffee price
+
+
+    return 0;
+}
